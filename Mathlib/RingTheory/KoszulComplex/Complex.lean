d8ccb7054d24410482a8b8ff02c523515b1fd49d
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-06-05 16:38:15 +0800

    remove wrong attempt

--- Mathlib/RingTheory/KoszulComplex/Complex.lean
+++ Mathlib/RingTheory/KoszulComplex/Complex.lean
@@ -173,28 +173,9 @@ end specialX
 
 section basechange
 
-variable (S : Type (max u v)) [CommRing S] [Algebra R S] {l : List R} {l' : List S}
+variable {S : Type (max u v)} [CommRing S] [Algebra R S] {l : List R} {l' : List S}
 
 open TensorProduct
-
-variable {S}
-
-noncomputable def Set.powersetCardEquiv (eqmap : l.map (algebraMap R S) = l') (i : ℕ) :
-    Set.powersetCard (Fin l'.length) i ≃ Set.powersetCard (Fin l.length) i where
-  toFun := Set.powersetCard.map i (finCongr (by simp [← eqmap])).toEmbedding
-  invFun := Set.powersetCard.map i (finCongr (by simp [← eqmap])).toEmbedding
-  left_inv x := by
-    ext
-    simp
-  right_inv x := by
-    ext
-    simp
-
-noncomputable def exteriorPowerBaseChange_iso (eqmap : l.map (algebraMap R S) = l') (i : ℕ) :
-    ⋀[S]^i (Fin l'.length → S) ≃ₗ[S] S ⊗[R] ⋀[R]^i (Fin l.length → R) :=
-  Module.Basis.equiv ((Pi.basisFun S (Fin l'.length)).exteriorPower i)
-    (((Pi.basisFun R (Fin l.length)).exteriorPower i).baseChange S)
-    (Set.powersetCardEquiv eqmap i)
 
 end basechange
 
